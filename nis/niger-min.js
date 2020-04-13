6a8eb8d2b7717ae92ada7a8e3e2ed3c4bdd33094
Said Abdulsalam <42839808+saidabdul80@users.noreply.github.com>
2020-04-13 09:33:13 +0100

    first upload

---
+++ nis/niger-min.js
@@ -0,0 +1,474 @@
+
+var ngjsconfig = {
+    "ngjs1":{
+        "hover": "MOKWA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs2":{
+        "hover": "EDATI",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs3":{
+        "hover": "LAVUN",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs4":{
+        "hover": "KATCHA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs5":{
+        "hover": "LAPAI",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs6":{
+        "hover": "EDATI",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs7":{
+        "hover": "GURARA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs8":{
+        "hover": "EDATI",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs9":{
+        "hover": "BIDA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs10":{
+        "hover": "GBAKO",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs11":{
+        "hover": "BOSSO",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs12":{
+        "hover": "MUYA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs13":{
+        "hover": "CHANCHAGA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs14":{
+        "hover": "SHIRORO",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs15":{
+        "hover": "WUSHISHI",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs16":{
+        "hover": "RAFI",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs17":{
+        "hover": "MARIGA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs18":{
+        "hover": "MAGAMA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs19":{
+        "hover": "AGWARA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs20":{
+        "hover": "MASHEGU",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs21":{
+        "hover": "BORGU",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs22":{
+        "hover": "KONTAGORA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs23":{
+        "hover": "RIJAU",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs24":{
+        "hover": "AGAI",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs25":{
+        "hover": "SULEJA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "ngjs26":{
+        "hover": "SULEJA",
+        "url": "#",
+        "target": "same_window",
+        "upColor": "#FFFFF3",
+        "overColor": "#ECFFB3",
+        "downColor": "#cae9af",
+        "active": !0
+    },
+    "general": {
+        "borderColor": "#9CA8B6",
+        "visibleNames": "#adadad"
+    }
+};
+
+function isTouchEnabled() {
+    return "ontouchstart" in window || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+}
+
+jQuery(function() {
+    jQuery("path[id^=ngjs]").each(function(i, e) {
+        ngaddEvent(jQuery(e).attr("id"));
+    });
+});
+
+function ngaddEvent(id, relationId) {
+    var _obj = jQuery("#" + id);
+    var arr = id.split("");
+    var _Textobj = jQuery("#" + id + "," + "#nis" + arr.slice(4).join(""));
+    jQuery("#" + [ "visnames" ]).attr({
+        fill: ngjsconfig.general.visibleNames
+    });
+    _obj.attr({
+        fill: ngjsconfig[id].upColor,
+        stroke: ngjsconfig.general.borderColor
+    });
+    _Textobj.attr({
+        cursor: "default"
+    });
+    if (ngjsconfig[id].active === !0) {
+        _Textobj.attr({
+            cursor: "pointer"
+        });
+        _Textobj.hover(function() {
+            jQuery("#jstip").show().html(ngjsconfig[id].hover);
+            _obj.css({
+                fill: ngjsconfig[id].overColor
+            });
+        }, function() {
+            jQuery("#jstip").hide();
+            jQuery("#" + id).css({
+                fill: ngjsconfig[id].upColor
+            });
+        });
+        if (ngjsconfig[id].target !== "none") {
+            _Textobj.mousedown(function() {
+                jQuery("#" + id).css({
+                    fill: ngjsconfig[id].downColor
+                });
+            });
+        }
+        _Textobj.mouseup(function() {
+            jQuery("#" + id).css({
+                fill: ngjsconfig[id].overColor
+            });
+            if (ngjsconfig[id].target === "new_window") {
+                window.open(ngjsconfig[id].url);
+            } else if (ngjsconfig[id].target === "same_window") {
+                window.parent.location.href = ngjsconfig[id].url;
+            } else if (ngjsconfig[id].target === "modal") {
+                jQuery(ngjsconfig[id].url).modal("show");
+            }
+        });
+        _Textobj.mousemove(function(e) {
+            var x = e.pageX + 10, y = e.pageY + 15;
+            var tipw = jQuery("#jstip").outerWidth(), tiph = jQuery("#jstip").outerHeight(), x = x + tipw > jQuery(document).scrollLeft() + jQuery(window).width() ? x - tipw - 20 * 2 : x;
+            y = y + tiph > jQuery(document).scrollTop() + jQuery(window).height() ? jQuery(document).scrollTop() + jQuery(window).height() - tiph - 10 : y;
+            jQuery("#jstip").css({
+                left: x,
+                top: y
+            });
+        });
+        if (isTouchEnabled()) {
+            _Textobj.on("touchstart", function(e) {
+                var touch = e.originalEvent.touches[0];
+                var x = touch.pageX + 10, y = touch.pageY + 15;
+                var tipw = jQuery("#jstip").outerWidth(), tiph = jQuery("#jstip").outerHeight(), x = x + tipw > jQuery(document).scrollLeft() + jQuery(window).width() ? x - tipw - 20 * 2 : x;
+                y = y + tiph > jQuery(document).scrollTop() + jQuery(window).height() ? jQuery(document).scrollTop() + jQuery(window).height() - tiph - 10 : y;
+                jQuery("#" + id).css({
+                    fill: ngjsconfig[id].downColor
+                });
+                jQuery("#jstip").show().html(ngjsconfig[id].hover);
+                jQuery("#jstip").css({
+                    left: x,
+                    top: y
+                });
+            });
+            _Textobj.on("touchend", function() {
+                jQuery("#" + id).css({
+                    fill: ngjsconfig[id].upColor
+                });
+                if (ngjsconfig[id].target === "new_window") {
+                    window.open(ngjsconfig[id].url);
+                } else if (ngjsconfig[id].target === "same_window") {
+                    window.parent.location.href = ngjsconfig[id].url;
+                } else if (ngjsconfig[id].target === "modal") {
+                    jQuery(ngjsconfig[id].url).modal("show");
+                }
+            });
+        }
+    }
+}
+
+var pins_config = {
+    "pins": [ {
+        "shape": "circle",
+        "hover": "<b><u>MINNA</u></b><br>Write any text and load images<br><img src='assets/images/example.png'>",
+        "pos_X": 1120,
+        "pos_Y": 378,
+        "size": 18,
+        "outline": "#000080",
+        "upColor": "#1a1aff",
+        "overColor": "#66d9ff",
+        "url": "#",
+        "target": "new_window",
+        "active": !0
+    }]
+};
+
+function isTouchEnabled() {
+    return "ontouchstart" in window || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+}
+
+jQuery(function() {
+    var pins_len = pins_config.pins.length;
+    if (pins_len > 0) {
+        var xmlns = "http://www.w3.org/2000/svg";
+        var tsvg_obj = document.getElementById("ngjspins");
+        var svg_circle, svg_rect;
+        for (var i = 0; i < pins_len; i++) {
+            if (pins_config.pins[i].shape === "circle") {
+                svg_circle = document.createElementNS(xmlns, "circle");
+                svg_circle.setAttributeNS(null, "cx", pins_config.pins[i].pos_X + 1);
+                svg_circle.setAttributeNS(null, "cy", pins_config.pins[i].pos_Y + 1);
+                svg_circle.setAttributeNS(null, "r", pins_config.pins[i].size / 2);
+                svg_circle.setAttributeNS(null, "fill", "rgba(0, 0, 0, 0.5)");
+                tsvg_obj.appendChild(svg_circle);
+                svg_circle = document.createElementNS(xmlns, "circle");
+                svg_circle.setAttributeNS(null, "cx", pins_config.pins[i].pos_X);
+                svg_circle.setAttributeNS(null, "cy", pins_config.pins[i].pos_Y);
+                svg_circle.setAttributeNS(null, "r", pins_config.pins[i].size / 2);
+                svg_circle.setAttributeNS(null, "fill", pins_config.pins[i].upColor);
+                svg_circle.setAttributeNS(null, "stroke", pins_config.pins[i].outline);
+                svg_circle.setAttributeNS(null, "stroke-width", 1);
+                svg_circle.setAttributeNS(null, "id", "ngjspins_" + i);
+                tsvg_obj.appendChild(svg_circle);
+                ngjsAddEvent(i);
+            } else if (pins_config.pins[i].shape === "square") {
+                svg_rect = document.createElementNS(xmlns, "rect");
+                svg_rect.setAttributeNS(null, "x", pins_config.pins[i].pos_X - pins_config.pins[i].size / 2 + 1);
+                svg_rect.setAttributeNS(null, "y", pins_config.pins[i].pos_Y - pins_config.pins[i].size / 2 + 1);
+                svg_rect.setAttributeNS(null, "width", pins_config.pins[i].size);
+                svg_rect.setAttributeNS(null, "height", pins_config.pins[i].size);
+                svg_rect.setAttributeNS(null, "fill", "rgba(0, 0, 0, 0.5)");
+                tsvg_obj.appendChild(svg_rect);
+                svg_rect = document.createElementNS(xmlns, "rect");
+                svg_rect.setAttributeNS(null, "x", pins_config.pins[i].pos_X - pins_config.pins[i].size / 2);
+                svg_rect.setAttributeNS(null, "y", pins_config.pins[i].pos_Y - pins_config.pins[i].size / 2);
+                svg_rect.setAttributeNS(null, "width", pins_config.pins[i].size);
+                svg_rect.setAttributeNS(null, "height", pins_config.pins[i].size);
+                svg_rect.setAttributeNS(null, "fill", pins_config.pins[i].upColor);
+                svg_rect.setAttributeNS(null, "stroke", pins_config.pins[i].outline);
+                svg_rect.setAttributeNS(null, "stroke-width", 1);
+                svg_rect.setAttributeNS(null, "id", "ngjspins_" + i);
+                tsvg_obj.appendChild(svg_rect);
+                ngjsAddEvent(i);
+            }
+        }
+    }
+});
+
+function ngjsAddEvent(id) {
+    var obj = jQuery("#ngjspins_" + id);
+    if (pins_config.pins[id].active === !0) {
+        obj.attr({
+            cursor: "pointer"
+        });
+        obj.hover(function() {
+            jQuery("#jstip").show().html(pins_config.pins[id].hover);
+            obj.css({
+                fill: pins_config.pins[id].overColor
+            });
+        }, function() {
+            jQuery("#jstip").hide();
+            obj.css({
+                fill: pins_config.pins[id].upColor
+            });
+        });
+        obj.mouseup(function() {
+            obj.css({
+                fill: pins_config.pins[id].overColor
+            });
+            if (pins_config.pins[id].target === "new_window") {
+                window.open(pins_config.pins[id].url);
+            } else if (pins_config.pins[id].target === "same_window") {
+                window.parent.location.href = pins_config.pins[id].url;
+            } else if (pins_config.pins[id].target === "modal") {
+                jQuery(pins_config.pins[id].url).modal("show");
+            }
+        });
+        obj.mousemove(function(e) {
+            var x = e.pageX + 10, y = e.pageY + 15;
+            var tipw = jQuery("#jstip").outerWidth(), tiph = jQuery("#jstip").outerHeight(), x = x + tipw > jQuery(document).scrollLeft() + jQuery(window).width() ? x - tipw - 20 * 2 : x;
+            y = y + tiph > jQuery(document).scrollTop() + jQuery(window).height() ? jQuery(document).scrollTop() + jQuery(window).height() - tiph - 10 : y;
+            jQuery("#jstip").css({
+                left: x,
+                top: y
+            });
+        });
+        if (isTouchEnabled()) {
+
+            obj.on("touchstart", function(e) {
+                var touch = e.originalEvent.touches[0];
+                var x = touch.pageX + 10, y = touch.pageY + 15;
+                var tipw = jQuery("#jstip").outerWidth(), tiph = jQuery("#jstip").outerHeight(), x = x + tipw > jQuery(document).scrollLeft() + jQuery(window).width() ? x - tipw - 20 * 2 : x;
+                y = y + tiph > jQuery(document).scrollTop() + jQuery(window).height() ? jQuery(document).scrollTop() + jQuery(window).height() - tiph - 10 : y;
+                jQuery("#jstip").show().html(pins_config.pins[id].hover);
+                jQuery("#jstip").css({
+                    left: x,
+                    top: y
+                });
+            });
+            obj.on("touchend", function() {
+                jQuery("#" + id).css({
+                    fill: pins_config.pins[id].upColor
+                });
+                if (pins_config.pins[id].target === "new_window") {
+                    window.open(pins_config.pins[id].url);
+                } else if (pins_config.pins[id].target === "same_window") {
+                    window.parent.location.href = pins_config.pins[id].url;
+                } else if (pins_config.pins[id].target === "modal") {
+                    jQuery(pins_config.pins[id].url).modal("show");
+                }
+            });
+        }
+    }
+}
